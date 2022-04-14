6687d73c0a7656c3e5f4b14671db8f7ec279a758
codificando12 <planeta.chompi18@gmail.com>
2022-04-14 21:13:02 +0800

    Initial commit

---
+++ conversor-moneda/conversor_usd-ars.py
@@ -0,0 +1,7 @@
+dolares = input("¿Cuántos dólares tienes?: ")
+dolares = float(dolares)
+valor_ars = 200
+ars = dolares / valor_ars
+ars = round(ars, 2)
+ars = str(ars)
+print("Tienes $ " + ars + " pesos argentinos")
