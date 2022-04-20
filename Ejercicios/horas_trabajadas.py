e3ccbcd29dd278ae14a3daa64de2ce66fae8bd11
codificando12 <93510169+codificando12@users.noreply.github.com>
2022-04-20 14:16:45 +0800

    nuevos ejercicios y practicas

---
+++ Ejercicios/horas_trabajadas.py
@@ -0,0 +1,5 @@
+horas = int(input("¿Cuántas horas trabajas por día?: "))
+salario = int(input("¿Cuánto te pagan por hora?: "))
+total = horas * salario
+total = str(total)
+print("Te pagan un total de $" + total + " por día")
