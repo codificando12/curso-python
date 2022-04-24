5b852ca37b33588bb717ff2a6c1ce62c20e9929c
codificando12 <planeta.chompi18@gmail.com>
2022-04-24 14:23:36 +0800

    ejercicio completo

--- Ejercicios/intereses.py
+++ Ejercicios/intereses.py
@@ -1,10 +1,9 @@
 def run():
-    cap = input("¿Cuánto quieres invertir hoy?: ")
-    interes = input("¿Qué tasa de interes deseas? ")
-    
-    # years = input("¿Cuánto años quieres invertir? ")
-    print("Invirtiendo " + str(cap) + " a una tasa del " + str(interes) + " anual " + " en " 
-    + " vas a tener un total de " + str(int(cap) * (int(interes)/ 100) + cap))
+    cap = int(input("¿Cuánto quieres invertir hoy?: "))
+    interes = int(input("¿Qué tasa de interes deseas? "))
+    years = int(input("¿Cuánto años quieres invertir? "))
+    print("Invirtiendo " + str(cap) + " a una tasa del " + str(interes) + " anual " + "en " + str(years) + " año(s) vas a tener un total de " + str(round(cap 
+          * (interes / 100 + 1) ** years, 2)))
 
 
 if __name__ == "__main__":
