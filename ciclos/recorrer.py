9bb396369f67527025838fa2079e1aba6e6814e6
codificando12 <planeta.chompi18@gmail.com>
2022-05-02 22:38:57 +0800

    recorrer.py

---
+++ ciclos/recorrer.py
@@ -0,0 +1,12 @@
+def run():
+    # name = input("Write your name: ")
+    # for letter in name:
+    #     print(letter)
+
+    frase = input("Write a phrase: ")
+    for character in frase:
+        print(character.upper())
+        
+        
+if __name__ == "__main__":
+    run()
