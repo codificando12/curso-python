5ee2aa78a2417f1aabe43929e503a5061ef9d9e7
codificando12 <93510169+codificando12@users.noreply.github.com>
2022-05-04 22:30:43 +0800

    update desafio.py

--- ciclos/desafio.py
+++ ciclos/desafio.py
@@ -1,5 +1,21 @@
+def is_prime(number):
+
+    for i in range(1, number + 1):
+        if i == 1 or i == number:
+            return True
+        else:
+            return False
+        
+    
+
+
+
 def run():
-    pass
+    number = int(input("Write a numbre: "))
+    if is_prime(number):
+        print("It is a prime number")
+    else:
+        print("It isn't a prime number.")
 
 
 if __name__ == "__main__":
