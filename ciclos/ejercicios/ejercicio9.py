b6e27868656dea0199abd533c5cd486ebe79807d
codificando12 <93510169+codificando12@users.noreply.github.com>
2022-05-07 23:11:22 +0800

    update ejercicio9

--- ciclos/ejercicios/ejercicio9.py
+++ ciclos/ejercicios/ejercicio9.py
@@ -2,11 +2,13 @@ def run():
     times = 3
     password = input("Write a password: ")
     ask = input("What is your password? ")
-    while password == ask:
-        print("The password is correct")
-        
-        if password != ask:
-            print("the password is wrong, write another password: ")
+    while password != ask:
+        print("the password is wrong")
+        break
+    print(input("please write it again: "))
+
+    if password == ask:
+        print("the password is correct")
         
         
 
