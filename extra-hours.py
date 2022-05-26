b8b5739b401b0b2a9da01808a7d00071b1944f56
codificando12 <planeta.chompi18@gmail.com>
2022-05-26 16:46:12 +0800

    update

--- extra-hours.py
+++ extra-hours.py
@@ -1,9 +1,36 @@
+# def run():
+#     hours = float(input("How many hours did you work last week?: "))
+#     wage = float(input("How much is your salary per hour? "))
+#     if hours > 40:
+#         extra = hours - 40
+#         extra_wage = (extra * wage) * 1.5
+#         print("Working hours " + str(hours) + "\n" + 
+#               "Extra hours: " + str(extra) + "\n" +
+#               "Normal salary: " + str(wage * 40) + "\n" +
+#               "Extra salary: " + str(extra_wage) + "\n" +
+#               "Total for this week: " + str((wage * 40) + extra_wage))       
+#     else:
+#            print("Working hours " + str(hours) + "\n" + 
+#               "Extra hours: 0" + "\n" +
+#               "Normal salary: " + str(wage * 40) + "\n" +
+#               "Extra salary: 0" + "\n" +
+#               "Total for this week: " + str(wage * 40))  
+
+# if __name__ == "__main__":
+#     run()
+    
 def run():
-    hours = float(input("How many hours did you work last week?: "))
-    wage = float(input("How much is your salary per hour? "))
-    if hours > 40:
+    
+    def e_hours(hours, wage):
         extra = hours - 40
         extra_wage = (extra * wage) * 1.5
+        return extra_wage
+    
+    hours = float(input("How many hours did you work last week?: "))
+    wage = float(input("How much is your salary per hour? "))
+        
+    if hours > 40:
+        e_hours()
         print("Working hours " + str(hours) + "\n" + 
               "Extra hours: " + str(extra) + "\n" +
               "Normal salary: " + str(wage * 40) + "\n" +
@@ -18,4 +45,3 @@ def run():
 
 if __name__ == "__main__":
     run()
-    
--- score.py
+++ score.py
@@ -18,3 +18,8 @@ def run():
 
 if __name__ == "__main__":
     run()
+
+
+
+
+
