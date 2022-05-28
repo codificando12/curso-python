e44c667417f1f31c10397bd18b96aad72a6640ac
codificando12 <planeta.chompi18@gmail.com>
2022-05-28 13:50:16 +0800

    update

---
+++ numbersex.py
@@ -0,0 +1,20 @@
+def run():
+    nums = []
+    while True:
+
+        num = input('Write a number: ')
+        try:
+            numb = int(num)
+            numb = nums.append(numb)
+            
+        except:
+            if num == "exit":
+                break
+            print("Write a valid number")
+        
+        
+    print(sum(nums))
+    
+    
+    if __name__ == "__main__":
+        run()    
--- score.py
+++ score.py
@@ -1,36 +1,38 @@
-# def run():
-#     score = input("write your score from 0.0 to 1.0 to know if you pass it: ")
+def run():
+    score = input("write your score from 0.0 to 1.0 to know if you pass it: ")
     
-#     def scores(point):
-#         try:
-#             point = float(score)
-#             if point >= 0.9:
-#                 print("Outstanding")
-#             elif point >= 0.8:
-#                 print("Remarkable")
-#             elif point >= 0.7:
-#                 print("Good")
-#             elif point >= 0.6:
-#                 print("Enought")
-#             else:
-#                 print("Insufficient")
-#         except:
-#             print("Please write a number from 0.0 to 1.0")
+    def scores(point):
+        try:
+            point = float(score)
+            if point >= 0.9:
+                print("Outstanding")
+            elif point >= 0.8:
+                print("Remarkable")
+            elif point >= 0.7:
+                print("Good")
+            elif point >= 0.6:
+                print("Enought")
+            else:
+                print("Insufficient")
+        except:
+            print("Please write a number from 0.0 to 1.0")
 
-#     scores(score)
-# if __name__ == "__main__":
-#     run()
+    scores(score)
+if __name__ == "__main__":
+    run()
 
 
-found = False
-print('Before ', found)
-for value in [9, 41, 12, 3, 74, 15]:
-    if value == 3:
-        found = True
+# found = False
+# print('Before ', found)
+# for value in [9, 41, 12, 3, 74, 15]:
+#     if value == 3:
+#         found = True
         
-    print(found, value)
-print('After', found)
+#     print(found, value)
+# print('After', found)
 
+
+ 
 
 
 
